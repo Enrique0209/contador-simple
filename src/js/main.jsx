7b87df2c02d86ae7924b8a29cf626caf30b91cab
Jessica Pinta <enrique.glez.jai@gmail.com>
2026-03-09 10:33:38 +0100

    version inicial

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,21 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let num1 = 0;
+
+setInterval(function () {
+
+  num1++
+//si num1 es > 9 => empezar a contar el siguiente numero y volver num1 a cero
+//si num2 es > 9 
+console.log(num1);
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Home digito1={num1} />
+    </React.StrictMode>,
+  )
+}, 1000);
+
+  
+ 
