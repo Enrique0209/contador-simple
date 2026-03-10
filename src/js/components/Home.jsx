75c12dafbead1083aec0f3d38be438e5eae09681
Jessica Pinta <enrique.glez.jai@gmail.com>
2026-03-10 20:46:30 +0100

    Versión final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = ({ digito1 }) => {
+const Home = ({ digito1, digito2, digito3, digito4, digito5, digito6 }) => {
 	return (
-		<div className="text-center">
-			<div>
-				<h1 styler={{ fontSize: "100px"}}>{digito1}</h1>	
+		<div className="container text-center mt-5">
+			<div className="d-flex justify-content-center">
+
+				<div className="card bg-dark text-white p-3 m-1">
+					<i className="fa-solid fa-clock fa-2x"></i>
+				</div>
+
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito1}</div>
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito2}</div>
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito3}</div>
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito4}</div>
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito5}</div>
+				<div className="card bg-dark text-white p-3 m-1 fs-1">{digito6}</div>
+
 			</div>
-		
-		
-			    
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,20 +11,32 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-let num1 = 0;
+let counter = 0;
 
 setInterval(function () {
 
-  num1++
-//si num1 es > 9 => empezar a contar el siguiente numero y volver num1 a cero
-//si num2 es > 9 
-console.log(num1);
+  counter++
 
-  ReactDOM.createRoot(document.getElementById('root')).render(
+  const digito1 = Math.floor(counter / 100000) % 10;
+  const digito2 = Math.floor(counter / 10000) % 10;
+  const digito3 = Math.floor(counter / 1000) % 10;
+  const digito4 = Math.floor(counter / 100) % 10;
+  const digito5 = Math.floor(counter / 10) % 10;
+  const digito6 = Math.floor(counter / 1) % 10;
+
+   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <Home digito1={num1} />
+      <Home 
+        digito1={digito1}
+        digito2={digito2}
+        digito3={digito3}
+        digito4={digito4}
+        digito5={digito5}
+        digito6={digito6}
+      />
     </React.StrictMode>,
   )
+
 }, 1000);
 
   
